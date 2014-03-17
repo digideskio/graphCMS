f7799496fab7b1eefb8b7fab587a1f1529a42e5a
Axel Dürkop <github@trainxl.de>
2014-03-17 13:24:11 +0100

    Abfrage bei Neo4J mit Anzeige aller Suchergebnisse

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,16 +3,69 @@
 angular.module('graphCmsApp')
     .controller('MainCtrl', ['$scope','entryService', function ($scope, entryService) {
 
-        $scope.save = function () {
-            console.log($scope.title, $scope.description);
+        $scope.title        = '';
+        $scope.description  = '';
 
-            // {   "query" : "CREATE (e:Entry {date: "2014-03-13", title: "{title}", description: "{description}"})",   "params" : { "title": "' + $scope.title + '", "description": "' + $scope.description + '" } }
-            entryService.saveEntry($scope.title)
+        $scope.data = {
+            title: $scope.title,
+            description: $scope.description
+        };
+
+        $scope.save = function () {
+
+            console.log($scope.data);
+
+            entryService.saveEntry($scope.data)
                 .success(function(data) {
-                    console.log('Alles gut: ' +  data.message);
+                    console.log(data.message);
+                    $scope.data = {};
+                    $scope.searchResult = [];
                 })
                 .error(function(err) {
                     console.log(err);
                 });
           };
+
+        $scope.getAll = function() {
+            entryService.getAll()
+                .success(function(data) {
+                    $scope.all = data;
+                })
+                .error(function(err) {
+                    console.log(err);
+                });
+        }
+
+        $scope.search = function() {
+            entryService.search(_getTitleWords())
+                .success(function(data) {
+                    $scope.searchResult = data.result;
+                })
+                .error(function(err) {
+                    console.log(err);
+                });
+        };
+
+        var _getTitleWords = function() {
+
+            // Die Wörter des title
+            var titleTerms = $scope.data.title.split(' ');
+
+            // Die Wörter der description
+            var descTerms = $scope.data.description.split(' ');
+
+            titleTerms = titleTerms.join('.*)|(.*');
+            titleTerms= '"(.*' + titleTerms + '.*)"';
+
+            descTerms = descTerms.join('.*)|(.*');
+            descTerms= '"(.*' + descTerms + '.*)"';
+
+            console.log(titleTerms);
+            console.log(descTerms);
+
+            return {
+                titleTerms: titleTerms,
+                descTerms:  descTerms
+            }
+        };
       }]);
--- app/scripts/services/services.js
+++ app/scripts/services/services.js
@@ -9,8 +9,8 @@ angular.module('graphCmsApp.services', [])
             // that calls the Github API using JSONP
             return $http({
                 method: 'POST',
-                url: api_url + '/save',
-                data: { title: entry }
+                url: api_url + '/node/save',
+                data: entry
             }).success(function(answer) {
                 return answer;
                 console.log('Success');
@@ -19,9 +19,38 @@ angular.module('graphCmsApp.services', [])
             });
         };
 
+        var runGetAllRequest = function() {
+            return $http({
+                method: 'GET',
+                url: api_url + '/node/all'
+            }).success(function(answer){
+                return answer;
+            }).error(function(err) {
+                console.log(err);
+            });
+        }
+
+        var runSearchRequest = function(searchString) {
+            return $http({
+                method: 'POST',
+                url: api_url + '/node/search',
+                data: searchString
+            }).success(function(answer){
+                return answer;
+            }).error(function(err) {
+                console.log(err);
+            });
+        }
+
         return {
             saveEntry: function(entry) {
                 return runSaveRequest(entry);
+            },
+            getAll: function() {
+                return runGetAllRequest();
+            },
+            search: function(searchString) {
+                return runSearchRequest(searchString);
             }
         };
 }]);
