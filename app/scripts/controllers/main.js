a1961d220cfb21c2696ae8a4d0937e83c7e2d5c6
Axel Dürkop <github@trainxl.de>
2014-03-18 00:00:51 +0100

    kleine Aenderungen

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,24 +1,34 @@
 'use strict';
 
 angular.module('graphCmsApp')
-    .controller('MainCtrl', ['$scope','entryService', function ($scope, entryService) {
+    .controller('MainCtrl', ['$scope','entryService', function ($scope,entryService) {
 
-        $scope.title        = '';
-        $scope.description  = '';
 
-        $scope.data = {
-            title: $scope.title,
-            description: $scope.description
+        var data = {
+            title: '',
+            description: '',
+            relations : []
         };
 
+        $scope.data = data;
+
+        /**
+         * Speichert einen Eintrag
+         */
         $scope.save = function () {
 
-            console.log($scope.data);
+            // Relations extrahieren und zusammenstellen
+            for ( var i = 0; i < $scope.searchResult.length; i++) {
+                if ($scope.searchResult[i].relations) {
+                    $scope.data.relations.push($scope.searchResult[i]._id);
+                }
+            }
 
+            // Ruft den Service
             entryService.saveEntry($scope.data)
-                .success(function(data) {
-                    console.log(data.message);
-                    $scope.data = {};
+                .success(function(answer) {
+                    console.log(answer.message);
+                    _resetData();
                     $scope.searchResult = [];
                 })
                 .error(function(err) {
@@ -26,27 +36,35 @@ angular.module('graphCmsApp')
                 });
           };
 
-        $scope.getAll = function() {
-            entryService.getAll()
-                .success(function(data) {
-                    $scope.all = data;
-                })
-                .error(function(err) {
-                    console.log(err);
-                });
+        var _resetData = function() {
+            $scope.data.title = '';
+            $scope.data.description = '';
+            $scope.data.relations = [];
         }
 
+        /**
+         * Suchfunktion: holt Nodes, in denen die Wörter aus Titel und
+         * Beschreibung vorkommen
+         */
         $scope.search = function() {
             entryService.search(_getTitleWords())
-                .success(function(data) {
-                    $scope.searchResult = data.result;
+                .success(function(answer) {
+                    $scope.searchResult = answer.result;
                 })
                 .error(function(err) {
                     console.log(err);
                 });
         };
 
+        /**
+         * Hilfsfunktion: Erstellt Strings in Form von regulären Ausdrücken,
+         * die in den Cypher-String eingebaut werden können.
+         * @returns {{titleTerms: (*|Array), descTerms: (*|Array)}}
+         * @private
+         */
         var _getTitleWords = function() {
+
+            console.log($scope.data);
 
             // Die Wörter des title
             var titleTerms = $scope.data.title.split(' ');
@@ -68,4 +86,26 @@ angular.module('graphCmsApp')
                 descTerms:  descTerms
             }
         };
+
+        /**
+         * Holt alle Nodes
+         */
+        $scope.getAll = function() {
+            entryService.getAll()
+                .success(function(result) {
+                    $scope.all = result;
+                })
+                .error(function(err) {
+                    console.log(err);
+                });
+        }
+
+        /**
+         * @deprecated
+         * @param id
+         */
+        $scope.refreshRelationList = function(id) {
+            console.log($scope.entryform.relSelection[0].$viewValue);
+        }
+
       }]);
