4c061dfaa8a6b4cc9456839d2a033ead30c8f93c
Axel Dürkop <github@trainxl.de>
2014-03-20 17:09:14 +0100

    Fixtures geschrieben

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ require('./lib/routes')(app);
 // Start server
 app.listen(config.port, function () {
   console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
+
+  // Database fixtures
+  require('./lib/fixtures/loadFixtures');
+
 });
 
 // Expose app
